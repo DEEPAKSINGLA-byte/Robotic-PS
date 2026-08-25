84188da1096adc0388a7cd4ff553244ea2105299
deepaksingla8059924923-byte <239532455+deepaksingla8059924923-byte@users.noreply.github.com>
2026-08-25 14:33:48 +0530

    added the fast_sam

--- yolo_fast_sam.py
+++ yolo_fast_sam.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from ultralytics import YOLO
+from mobile_sam import sam_model_registry, SamPredictor
 import numpy as np
 import cv2
 
@@ -23,10 +24,8 @@ class YoloNode(Node):
 
         self.model = YOLO("yolov8x-worldv2.pt")
 
-        # COCO's 80 standard object classes.
-        # YOLO-World also uses these as its default offline vocabulary,
-        # but we define them explicitly here.
-        # Curated 50-class vocabulary for common indoor/household objects.
+        self.sam = sam_model_registry["vit_t"](checkpoint="mobile_sam.pt")
+        self.predictor = SamPredictor(self.sam)
         self.model.set_classes([
             "person",
             "chair",
@@ -82,19 +81,75 @@ class YoloNode(Node):
 
     def image_callback(self, msg):
 
-    # 1. ROS Image → OpenCV image
-        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        frame = self.bridge.imgmsg_to_cv2(
+            msg,
+            desired_encoding='bgr8'
+        )
 
-        # 2. Run YOLO
         results = self.model(frame)
 
-        # 3. Draw detections on the image
-        annotated_frame = results[0].plot()
 
-        # 4. Display the annotated image
-        cv2.imshow("YOLO Detection", annotated_frame)
+        visualization = frame.copy()
+        if len(results[0].boxes) > 0:
+            rgb_frame = cv2.cvtColor(
+            frame,
+            cv2.COLOR_BGR2RGB
+            )
+            self.predictor.set_image(rgb_frame)
+            for box in results[0].boxes:
+
+                box_xyxy = box.xyxy[0].cpu().numpy()
+
+                masks, scores, logits = self.predictor.predict(
+                    box=box_xyxy,
+                    multimask_output=False
+                )
+
+                mask = masks[0]
+
+
+                overlay = visualization.copy()
+                overlay[mask] = (0, 255, 0)
+
+                visualization = cv2.addWeighted(
+                    visualization,
+                    0.7,
+                    overlay,
+                    0.3,
+                    0
+                )
+                x1, y1, x2, y2 = box_xyxy.astype(int)
+
+                cv2.rectangle(
+                    visualization,
+                    (x1, y1),
+                    (x2, y2),
+                    (255, 0, 0),
+                    2
+                )
+                class_id = int(box.cls[0])
+                confidence = float(box.conf[0])
+                class_name = self.model.names[class_id]
+
+                label = f"{class_name} {confidence:.2f}"
+
+                cv2.putText(
+                    visualization,
+                    label,
+                    (x1, max(y1 - 10, 20)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6,
+                    (255, 0, 0),
+                    2
+                )
+
+        cv2.imshow(
+            "YOLO + MobileSAM",
+            visualization
+        )
+
         cv2.waitKey(1)
-        
+
 
 def main(args=None):
     rclpy.init(args=args)
